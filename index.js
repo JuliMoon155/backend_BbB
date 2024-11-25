65c6a943c3a4ee8a0793a64adb1160b279b974d2
Juliana <jumemu2002@hotmail.com>
2024-11-25 17:44:24 -0500

    configuracion cors

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const express = require('express');
-const app = express();
 const dotenv = require('dotenv');
 const { Pool } = require('pg');
+const cors = require('cors');
 
 dotenv.config();
 
+const app = express();
+const PORT = process.env.PORT;
+const connectionString = process.env.DATABASE_URL;
+
+// Configuración de CORS
+const corsOptions = {
+    origin: 'https://ladrilloaladrillo-production.up.railway.app',  // La URL de tu frontend (aquí debe ir tu URL de frontend en Railway)
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  };
+
+app.use(cors(corsOptions));
+
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
+    connectionString: connectionString,
+  ssl: {
+    rejectUnauthorized: false, 
+  },
+  });
+
 
 app.use(express.json());
 
@@ -15,7 +32,6 @@ app.get('/', (req, res) => {
   res.send('Servidor funcionando correctamente');
 });
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
